52a400c70df2966c9de08e9dc370fea169df2bf8
ndurbin <nick.durbin@gmail.com>
2019-11-11 09:06:34 -0500

    Initial commit. Setup the middleware in the server.js.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = 'express';
-
 const server = express();
+
+const userRouter = require('./users/userRouter')
+
+server.use(express.json())
+server.use(logger)
+server.use('api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +14,10 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(
+    `${new.Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  )
+  next()
 };
 
 module.exports = server;
