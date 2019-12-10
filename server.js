16a8dcd6ee461988dbe976ae1aafa17c833d9e33
ndurbin <nick.durbin@gmail.com>
2019-12-10 11:52:04 -0500

    Added a 404 and 500 error and status.

--- server.js
+++ server.js
@@ -3,13 +3,26 @@ const { logger } = require('./middleware/logger')
 const server = express();
 
 const userRouter = require('./users/userRouter')
+const postRouter = require('./posts/postRouter')
 
-server.use(express.json())
 server.use(logger)
+server.use(express.json())
 server.use('api/users', userRouter)
+server.use('api/posts', postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
+
+server.use((res, req) => {
+  res.status(404).json({ message: "You have ventured into the abyss!"})
+})
+
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({
+    message: "An error occurred, please try again later."
+  })
+})
 
 module.exports = server;
