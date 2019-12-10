22c66cdf25d9ca2d317b675cb0eb20c6a7ee9a44
nick.durbin <nick.durbin@gmail.com>
2019-12-10 18:08:50 -0500

    Functionality to retrieve all posts and by post Id

--- api/routes/posts/postRouter.js
+++ api/routes/posts/postRouter.js
@@ -1,27 +1,28 @@
 const express = require('express')
-
+const posts = require('./postDb')
+const { validateUserId, validateUser, validatePost, validatePostId } = require('../../middleware/validate');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+  posts.get()
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(error => {
+      next(error)
+    })
+});
+
+router.get('/:id', validatePostId(), (req, res, next) => {
+  res.json(req.post)
+});
+
+router.delete('/:id', (req, res, next) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
 
 });
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-// custom middleware
-
-function validatePostId(req, res, next) {
-
-};
 
 module.exports = router;
