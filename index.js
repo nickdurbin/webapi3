125e346b727df09f63258d6301931411d7850302
nick.durbin <nick.durbin@gmail.com>
2019-12-09 22:06:10 -0500

    Added CRA and the env for the index.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const server = require('./server')
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`)
