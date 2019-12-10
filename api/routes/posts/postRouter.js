bd4b9dea4d942aa8c1a8f6fd4fc96af89d91042a
nick.durbin <nick.durbin@gmail.com>
2019-12-10 18:12:52 -0500

    Basic logic for delete and update in posts.

--- api/routes/posts/postRouter.js
+++ api/routes/posts/postRouter.js
@@ -17,12 +17,28 @@ router.get('/:id', validatePostId(), (req, res, next) => {
   res.json(req.post)
 });
 
-router.delete('/:id', (req, res, next) => {
-
+router.delete('/:id', validatePostId(), (req, res, next) => {
+  posts.remove(req.user.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: `You have successfully deleted ${count} records.` })
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 });
 
-router.put('/:id', (req, res, next) => {
-
+router.put('/:id', validatePost(), validatePostId(), (req, res, next) => {
+  posts.update(req.params.id, req.body)
+  .then(post => {
+    if (post) {
+      res.status(200).json(post)
+    }
+  })
+  .catch(error => {
+    next(error)
+  })
 });
 
 module.exports = router;
