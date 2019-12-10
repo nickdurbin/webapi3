e22ee884018506cffbe292728598a2549eb041c3
ndurbin <nick.durbin@gmail.com>
2019-12-10 11:53:00 -0500

    Setup all user logic with the middleware.

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,17 @@
-const express = require('express')
+const express = require('express');
+const { validateUserId, validateUser } = require('../middleware/validate');
+const users = require('./userDb');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post('/', validateUser(), (req, res) => {
+  users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      next(error)
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -11,37 +19,45 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+  users.get()
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(error => {
+      next(error)
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId(), (req, res) => {
+  res.json(req.user)
 });
 
 router.get('/:id/posts', (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserId(), (req, res) => {
+  hubs.remove(req.user.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: `You have successfully deleted ${count} records.` })
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validateUserId(), validateUser(), (req, res) => {
+  users.update(req.params.id, req.body)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user)
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 });
-
-//custom middleware
-
-function validateUserId(req, res, next) {
-
-};
-
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
 
 module.exports = router;
