36db1f06c91bcc43c1b5a29746e11e698ad38df3
ndurbin <nick.durbin@gmail.com>
2019-12-10 11:02:24 -0500

    Moved the logic into a separate file for a logger.

---
+++ middleware/logger.js
@@ -0,0 +1,8 @@
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  )
+  next()
+};
+
+module.exports = logger;
