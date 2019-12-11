8790c120ebb7227c95c2860b52aa6b6425ae9258
nick.durbin <nick.durbin@gmail.com>
2019-12-10 20:30:23 -0500

    Added an index.css file for a style reset.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+import BG from './images/bg.jpg'
 
 function App() {
   const [users, setUsers] = useState([])
@@ -16,16 +18,44 @@ function App() {
 
 
   return (
-     <div className='mainContainer'>
+     <MainContainer>
        {users.map(user => {
          return (
-           <div className="userContainer" key={user.id}>
+           <User className="userContainer" key={user.id}>
              <h2>Name: {user.name}</h2>
-          </div>
+          </User>
          )
        })}
-     </div>
+     </MainContainer>
   )
 }
+
+const MainContainer = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  background-image: url(${BG});
+  background-repeat: no-repeat;
+  background-position: center;
+  object-fit: cover;
+`;
+
+const User = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20%;
+  max-height: 200px;
+  height: 100%;
+  margin: 1%;
+  padding: 1%;
+  background: white;
+  border: none;
+  border-radius: 1em;
+  box-shadow: 1px 3px 5px;
+`
 
 export default App;
