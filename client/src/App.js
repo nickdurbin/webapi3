6a7a2799fb00a1804b4e7e48e2d0f8f9a1f27a02
nick.durbin <nick.durbin@gmail.com>
2019-12-10 19:34:10 -0500

    Added the very basic list of users to be displayed on the frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-    return (
-        <h1>Hello World</h1>
-    )
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/users')
+      .then(res => {
+        setUsers(res.data)
+      })
+      .catch(err => {
+        console.log(err, 'Users could not be found.')
+      })
+  })
+
+
+  return (
+     <div className='mainContainer'>
+       {users.map(user => {
+         return (
+           <div className="userContainer" key={user.id}>
+             <h2>Name: {user.name}</h2>
+          </div>
+         )
+       })}
+     </div>
+  )
 }
 
 export default App;
